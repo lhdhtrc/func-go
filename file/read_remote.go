fb192e8bfe75149a3036ff395045cd8ad9ed21b6
lhdht <15229684090@163.com>
2024-04-14 18:36:53 +0800

    feature: ReadRemote

---
+++ file/read_remote.go
@@ -0,0 +1,21 @@
+package file
+
+import (
+	"io"
+	"net/http"
+)
+
+func ReadRemote(url string) (*[]byte, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		if ce := Body.Close(); ce != nil {
+			err = ce
+		}
+	}(res.Body)
+
+	bytes, _ := io.ReadAll(res.Body)
+	return &bytes, err
+}
