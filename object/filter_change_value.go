87a55f74b39cff42bbd46a91df1bb09b78acde74
lhdht <15229684090@163.com>
2024-09-26 17:56:33 +0800

    fix: 修复结构体转换异常

--- object/filter_change_value.go
+++ object/filter_change_value.go
@@ -6,20 +6,39 @@ import (
 )
 
 // FilterChangeValue 过滤发生改变的值
-func FilterChangeValue[O any, N any](old O, new N, ignore *[]string) map[string]interface{} {
+func FilterChangeValue[O any, N any](old O, new N, ignore []string) map[string]interface{} {
+	ro := reflect.ValueOf(old)
+	rn := reflect.ValueOf(new)
+
+	if ro.Kind() != reflect.Struct && (ro.Kind() != reflect.Ptr || ro.Elem().Kind() != reflect.Struct) {
+		return nil
+	}
+	if rn.Kind() != reflect.Struct && (rn.Kind() != reflect.Ptr || rn.Elem().Kind() != reflect.Struct) {
+		return nil
+	}
+
+	if ro.Kind() == reflect.Ptr {
+		ro = ro.Elem()
+	}
+	if rn.Kind() == reflect.Ptr {
+		rn = rn.Elem()
+	}
+
 	variant := make(map[string]interface{})
 
-	ro := reflect.ValueOf(old).Elem()
-	rn := reflect.ValueOf(new).Elem()
 	for i := 0; i < rn.NumField(); i++ {
 		k := rn.Type().Field(i).Name
-		v := rn.Field(i)
 
-		if array.Include(*ignore, k) || v.IsZero() || ro.FieldByName(k).Interface() == v.Interface() {
+		if array.Include(ignore, k) || !ro.FieldByName(k).IsValid() {
 			continue
 		}
 
-		variant[k] = v.Interface()
+		nv := rn.Field(i).Interface()
+		ov := ro.FieldByName(k).Interface()
+
+		if !reflect.DeepEqual(ov, nv) {
+			variant[k] = nv
+		}
 	}
 
 	return variant
