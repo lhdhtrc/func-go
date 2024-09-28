d546486191e016d700def5a38bfe508448e6cdbd
lhdht <15229684090@163.com>
2024-09-28 23:03:36 +0800

    feature: 尝试将值转换为字符串，支持数字和字符串类型

--- str/core.go
+++ str/core.go
@@ -1,9 +1,24 @@
 package str
 
 import (
+	"reflect"
 	"strconv"
 	"strings"
 )
+
+// ToString 尝试将值转换为字符串，支持数字和字符串类型
+func ToString(v interface{}) string {
+	switch t := v.(type) {
+	case string:
+		return t
+	case int, int8, int16, int32, int64:
+		return strconv.FormatInt(reflect.ValueOf(t).Int(), 10)
+	case uint, uint8, uint16, uint32, uint64:
+		return strconv.FormatUint(reflect.ValueOf(t).Uint(), 10)
+	default:
+		return "" // 或返回错误
+	}
+}
 
 // StringToUintSlice 字符串转数字切片
 func StringToUintSlice(s string) []uint {
