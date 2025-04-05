dde57b7aa5a9e819bc3e7cb1cbe23e2ea5a6cc68
lhdht <15229684090@163.com>
2025-04-05 09:08:47 +0800

    fix: 修改

---
+++ arrary/filter.go
@@ -0,0 +1,12 @@
+package arrary
+
+// Filter 过滤函数
+func Filter[T any](array []T, fn func(index int, item T) bool) []T {
+	var temp []T
+	for index, item := range array {
+		if fn(index, item) {
+			temp = append(temp, item)
+		}
+	}
+	return temp
+}
